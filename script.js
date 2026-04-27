d1faa50c70aa1af822c5c5373358d96e02ef2ef1
GrntKim <grantjun0224@gmail.com>
2026-04-27 13:49:42 +0900

    completely implemented game logic for single-round play

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
 function getHumanChoice() {
-    var humanChoice = prompt('Make your choice').toLowerCase();
+    var humanChoice = prompt('Make your choice');
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+var humanScore = 0;
+var computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    var h = humanChoice.toLowerCase();
+    var c = computerChoice.toLowerCase();
+    if(h === c) {
+        console.log('Draw! ' + humanChoice + ' and ' + computerChoice);
+        return;
+    }
+    var lost = false;
+    if(h === 'rock') {
+        if(c === 'scissors') {
+            humanScore++;
+        } else {
+            computerScore++;
+            lost = true;
+        }
+    } else if(h === 'scissors') {
+        if(c === 'paper') {
+            humanScore++;
+        } else {
+            computerScore++;
+            lost = true;
+        }
+    } else if(h === 'paper') {
+        if(c === 'rock') {
+            humanScore++;
+        } else {
+            computerScore++;
+            lost = true;
+        }
+    }
+    if (lost) console.log('You lose! ' + computerChoice + ' beats ' + humanChoice)
+    else console.log('You Win! ' + humanChoice + ' beats ' + computerChoice);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
